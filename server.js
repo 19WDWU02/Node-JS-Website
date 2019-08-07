f555d1bc798a82d03522e4162ca9820c80948842
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-08-08 11:48:38 +1200

    added a post request from our index.html and uses the query string module to turn it to a js object

--- server.js
+++ server.js
@@ -1,32 +1,69 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+var qs = require('querystring');
 
 http.createServer(function(req, res){
   console.log(`${req.method} request for ${req.url}`);
-  if(req.url === '/'){
-    fs.readFile('./public/index.html', 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    })
-  } else if(req.url.match(/.css/)){
-    const cssPath = path.join(__dirname, 'public', req.url);
-    fs.readFile(cssPath, 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      res.end(data);
-    })
-  } else if(req.url === '/js/script.js'){
-    fs.readFile('./public/js/script.js', 'UTF-8', function(err, data){
-      if (err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/js'});
-      res.end(data);
-    })
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('404 file not found');
+
+  if(req.method === 'GET'){
+    if(req.url === '/'){
+      fs.readFile('./public/index.html', 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      })
+    } else if(req.url.match(/node_modules/)){
+      const modulePath = path.join(__dirname, req.url);
+      fs.readFile(modulePath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      })
+    } else if(req.url.match(/.css/)){
+      const cssPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(cssPath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(data);
+      })
+    } else if(req.url.match(/.js/)){
+      const jsPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(jsPath, 'UTF-8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/js'});
+        res.end(data);
+      })
+    } else if(req.url.match(/.jpeg/)){
+      const imgPath = path.join(__dirname, 'public', req.url);
+      fs.readFile(imgPath, function(err, data){
+        if(err) throw err;
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        res.end(data);
+      })
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('404 file not found');
+    }
+  } else if(req.method === 'POST'){
+    if(req.url === '/sendForm'){
+      let body = '';
+      req.on('data', function(data){
+        body += data;
+      })
+      req.on('end', function(){
+        console.log('at the end');
+        console.log(body.toString());
+        const formData = qs.parse(body.toString());
+        console.log(formData);
+      })
+
+    }
   }
+
+
+
+
 
 }).listen(3000);
 
